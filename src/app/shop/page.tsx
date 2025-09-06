9237d19b0b43e5cd72d8218e423d61bb42b25c25
Ritesh kumar <rav332211@gmail.com>
2025-09-06 10:15:34 +0000

    Try fixing this error: `Runtime Error: Error: useAddToCartDialog must be

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,11 +2,14 @@
 import { Suspense } from 'react';
 import { ShopPageClient } from '@/components/ShopPageClient';
 import ShopPageSkeleton from './loading';
+import { AddToCartDialog } from '@/components/AddToCartDialog';
 
 export default function ShopPage() {
   return (
     <Suspense fallback={<ShopPageSkeleton />}>
-      <ShopPageClient />
+      <AddToCartDialog>
+        <ShopPageClient />
+      </AddToCartDialog>
     </Suspense>
   );
 }
