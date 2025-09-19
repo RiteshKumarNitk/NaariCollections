382cae2aef52aebc9bea9ef2e35ac6f87a21f2f6
Ritesh kumar <rav332211@gmail.com>
2025-09-19 03:29:23 +0000

    Try fixing this error: `Runtime Error: Error: useFieldArray is not defin

--- src/app/admin/homepage/page.tsx
+++ src/app/admin/homepage/page.tsx
@@ -3,7 +3,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import Image from 'next/image';
